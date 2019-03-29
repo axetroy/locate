dff35f589ccacdb072f4d2fd7e731211169fe9e8
axetroy <troy450409405@gmail.com>
2019-03-29 13:48:47 +0800

    update

--- index.js
+++ index.js
@@ -6,10 +6,16 @@ const APP_KEY = process.env.APP_KEY || "";
 // Creating new HTTP server.
 http
   .createServer((req, res) => {
+    const ip =
+      (req.headers["x-forwarded-for"] || "").split(",").pop() ||
+      req.connection.remoteAddress ||
+      req.socket.remoteAddress ||
+      req.connection.socket.remoteAddress;
     axios
       .get("http://api.map.baidu.com/location/ip", {
         params: {
-          ak: APP_KEY
+          ak: APP_KEY,
+          ip
         }
       })
       .then(({ data }) => {
