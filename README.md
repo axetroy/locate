2dbbc94342e4bc2bcf6e3c1de5bc545284f1cf76
axetroy <troy450409405@gmail.com>
2019-03-29 14:06:27 +0800

    update

---
+++ README.md
@@ -0,0 +1,10 @@
+## 获取您的 IP 地理位置定位
+
+```bash
+# 获取你自己的IP定位
+curl http://locate.axetroy.xyz
+wget -qO- http://locate.axetroy.xyz
+
+# 获取指定的IP定位
+curl http://locate.axetroy.xyz?id=192.168.0.0
+```
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const http = require("http");
+const url = require("url");
 const axios = require("axios");
 const port = process.env.PORT || 3000;
 const APP_KEY = process.env.APP_KEY || "";
@@ -6,15 +7,22 @@ const APP_KEY = process.env.APP_KEY || "";
 // Creating new HTTP server.
 http
   .createServer((req, res) => {
+    const { query, pathname } = url.parse(req.url, true);
     const ip =
+      query.ip ||
       (req.headers["x-forwarded-for"] || "").split(",").pop() ||
       req.connection.remoteAddress ||
       req.socket.remoteAddress ||
       req.connection.socket.remoteAddress;
+    if (pathname === "/favicon.ico") {
+      res.writeHead(404);
+      res.end();
+      return;
+    }
     axios
       .get("http://api.map.baidu.com/location/ip", {
         params: {
-          ak: APP_KEY,
+          ak: query.ak || APP_KEY,
           ip
         }
       })
